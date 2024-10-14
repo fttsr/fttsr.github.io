799a2779cbeaea60cab634cb0ea5a91de0164df4
fttsr <fetteser.niox@gmail.com>
2024-10-14 16:38:19 +0300

    Initial commit

--- src/init.js
+++ src/init.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const init = () => {
 	const sizes = {
@@ -8,18 +7,19 @@ const init = () => {
 	};
 
 	const scene = new THREE.Scene();
+	const loader = new THREE.TextureLoader(); // Для заднего фона
+	loader.load(require('./grad2.jpg'), (texture) => {
+    	scene.background = texture;
+	})
 	const canvas = document.querySelector('.canvas');
 	const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 	scene.add(camera);
-
-	const controls = new OrbitControls(camera, canvas);
-	controls.enableDamping = true;
 
 	const renderer = new THREE.WebGLRenderer({ canvas });
 	renderer.setSize(sizes.width, sizes.height);
 	renderer.render(scene, camera);
 
-	return { sizes, scene, canvas, camera, renderer, controls };
+	return { sizes, scene, canvas, camera, renderer};
 };
 
 export default init;
--- src/script.js
+++ src/script.js
@@ -1,24 +1,108 @@
 import * as THREE from 'three';
-
 import init from './init';
-
 import './style.css';
 
-const { sizes, camera, scene, canvas, controls, renderer } = init();
+const { sizes, camera, scene, canvas, renderer } = init();
 
 camera.position.z = 3;
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+// Объект
+const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({
-	color: 'gray',
-	wireframe: true,
+    color: 'green',
+    wireframe: true,
 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+const clock = new THREE.Clock();
+
+// Полосы
+const stripWidth = 30;
+let stripHeight = 0.1;
+const maxNarrow = 3.5;
+let currNarrow = 2;
+
+// Функция создания полосы
+const createStrip = (yPos) => {
+    const stripGeometry = new THREE.PlaneGeometry(stripWidth, stripHeight);
+    const stripMaterial = new THREE.MeshBasicMaterial( {color: 'brown'} );
+    const strip = new THREE.Mesh(stripGeometry, stripMaterial);
+
+    strip.position.y = yPos;
+    strip.position.x = -Math.PI / 2 // Поворачиваем полосы для правильного отображения
+
+    return strip;
+}
+
+const topStrip = createStrip(1);
+const bottomStrip = createStrip(-1);
+scene.add(topStrip, bottomStrip);
+
+// Массив цветов
+const colors = ['red', 'cyan', 'blue', 'yellow', 'purple', 
+						'brown', 'cyan', 'white', 'orange']
+
+// Счётчик пойманных кубок
+let scoreCatched = 0;
+
+// Click-event
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+const handleClick = () => {
+    window.addEventListener('click', (event) => {
+        mouse.x = (event.clientX / sizes.width) * 2 - 1;
+        mouse.y = -(event.clientY / sizes.height) * 2 + 1;
+    
+        // Установка камеры
+        raycaster.setFromCamera(mouse, camera);
+    
+        // Проверка пересечении луча с кубом
+        const intersects = raycaster.intersectObject(cube);
+
+
+    
+        if (intersects.length > 0) {
+            // Находится ли куб между полосами
+            if (cube.position.y < topStrip.position.y + stripHeight / 2
+                && cube.position.y > bottomStrip.position.y - stripHeight / 2
+            ) {
+                alert('Поймал!');
+                scoreCatched++;
+                document.getElementById('score').innerText = `Поймано: ${scoreCatched}`;
+
+                cube.material.color.set(colors[Math.floor(Math.random() * colors.length)]);
+
+                // Сужаем полосы
+                if (currNarrow < maxNarrow) {
+                    currNarrow += -0.1;
+                    topStrip.position.y = (currNarrow / 2);
+                    bottomStrip.position.y = -(currNarrow / 2); 
+                }
+                
+                    
+            } else {
+                alert('Мимо!');
+                location.reload();
+            }
+        } else {
+            alert('Мимо!');
+            location.reload();
+        }
+    });
+};
+handleClick();
+
 
 const tick = () => {
-	controls.update();
 	renderer.render(scene, camera);
+
+    const elapsedTime = clock.getElapsedTime();
+
+    cube.position.x = Math.cos(elapsedTime) * 3;
+    cube.position.y = Math.sin(elapsedTime) * 2;
+
 	window.requestAnimationFrame(tick);
 };
 tick();
@@ -37,12 +121,4 @@ window.addEventListener('resize', () => {
 	renderer.setSize(sizes.width, sizes.height);
 	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 	renderer.render(scene, camera);
-});
-
-window.addEventListener('dblclick', () => {
-	if (!document.fullscreenElement) {
-		canvas.requestFullscreen();
-	} else {
-		document.exitFullscreen();
-	}
 });
